7b5096e06d990370ff13c3a3c01ef93a38338727
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-03-28 13:19:57 +0200

    add port to cors origin

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:5174"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
