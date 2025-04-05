eb048644ade1d7dfbcc3dd9348be094cd025f7c2
Rim-sherif <120836789+Rim-sherif@users.noreply.github.com>
2025-04-05 05:20:51 +0200

    update on getCourseById

--- src/modules/course/services/course.service.ts
+++ src/modules/course/services/course.service.ts
@@ -290,6 +290,20 @@ export const getCourseById = async (
     )
     .lookUp(
       {
+        from: "courses",
+        localField: "category._id",
+        foreignField: "categoryId",
+        as: "coursesInCategory",
+        isArray: true
+      }
+    )
+    .addStage({
+      $addFields: {
+        "category.totalCourses": { $size: "$coursesInCategory" }
+      }
+    })
+    .lookUp(
+      {
         localField: "_id",
         from: "sections",
         foreignField: "courseId",
